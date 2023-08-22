2aac4b407b11a93470b8cc1f9129033c0a85243c
cybermundo0 <139307875+cybermundo0@users.noreply.github.com>
2023-08-21 21:38:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,24 @@ BASE_DIR = '/app/created_directories'
 # File that contains a list of directory names
 WORD_LIST = 'top_directories.txt'
 
+
+# Serve the main page that provides the context about the app.
+# This is the landing page that users will see when they access the root URL.
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
+# List the directories that have been created.
+# This is a hidden route not explicitly mentioned in the main page, intended for users to discover through fuzzing.
 @app.route('/hidden/')
 def hidden_directories():
     directories = os.listdir(BASE_DIR)
     return render_template('directories.html', directories=directories)
 
+# Serve a static page for the specified directory.
+# If a directory matching the provided dirname exists, serve a static page for it. Otherwise, return a 404 error.
 @app.route('/<dirname>')
 def directory_page(dirname):
-    """
-    Serve a static page for the specified directory.
-    """
     # Check if the directory exists
     dir_path = os.path.join(BASE_DIR, dirname)
     if os.path.exists(dir_path):
@@ -32,13 +36,9 @@ def directory_page(dirname):
     else:
         return "Directory not found.", 404
 
+# Create the initial 30 directories inside the BASE_DIR.
+# This function is called once when the app starts to set up the directory environment.
 def create_initial_directories():
-    """
-    Create the initial 30 directories inside the BASE_DIR.
-
-    This function is called once when the app starts to set up 
-    the directory environment.
-    """
     # Check if the word list exists
     if not os.path.exists(WORD_LIST):
         raise FileNotFoundError(f"Word list '{WORD_LIST}' not found.")
@@ -52,13 +52,9 @@ def create_initial_directories():
     for directory in directory_names:
         os.makedirs(os.path.join(BASE_DIR, directory.strip()), exist_ok=True)
 
+# Periodically create a new directory every 30 minutes.
+# Stop creating new directories after a total of 100 directories have been created. This function is run in a separate thread.
 def periodic_directory_creation():
-    """
-    Periodically create a new directory every 30 minutes.
-
-    Stop creating new directories after a total of 100 directories
-    have been created. This function is run in a separate thread.
-    """
     while True:
         # Sleep for 30 minutes
         time.sleep(1800)
